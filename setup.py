97746ade27a665cc109a0f11ae1b063a35037547
Adam Bak <59940328+A-Bak@users.noreply.github.com>
2022-01-25 11:51:12 +0100

    Package setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="Webpage-Image-Downloader",
+    version="0.0.1",
+    author="ABak",
+    author_email="adam.bak753@gmail.com",
+    description="Tool for extracting and saving specific images from a website.",
+    packages=["cli"],
+)
