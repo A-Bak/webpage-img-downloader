50eab1f47f6250776b3fdf86b1fa03723d90c7cb
Adam Bak <59940328+A-Bak@users.noreply.github.com>
2022-02-07 12:08:48 +0100

    Added - Script takes url input from the clipboard when no input is provided by --url option

--- wid/run.py
+++ wid/run.py
@@ -1,12 +1,13 @@
 import click
 
 import imgscrape
+import pyperclip
 
 
 
 
 @click.command()
-@click.option('--url', default='https://duckduckgo.com/', help='Url of the website containing desired images.')
+@click.option('--url', default=None, help='Url of the website containing desired images.')
 @click.option('--target', default='./wid_images', help='Target directory used to store images.')
 @click.option('--img_regex', help='Regex for finding specific subset of images on the website.')
 def execute(url, target, img_regex):
@@ -15,7 +16,13 @@ def execute(url, target, img_regex):
     
     click.echo("Starting to parse {}...".format(url))
     
-    imgs = imgscrape.extract_images(url)
+    if url is None:
+        
+        imgs = imgscrape.extract_images(pyperclip.paste())
+        
+    else:
+        
+        imgs = imgscrape.extract_images(url)
     
     # Store images somewhere        
     
