9cfefdf57417b6647e616eb72d158c7639b1f293
Adam Bak <59940328+A-Bak@users.noreply.github.com>
2022-02-14 11:10:35 +0100

    Added - Downloading images with src.

--- wid/imgscrape.py
+++ wid/imgscrape.py
@@ -1,12 +1,16 @@
 import re
+import traceback
+
+from urllib.request import urlretrieve
 
 from selenium import webdriver
+from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 
 from webdriver_manager.chrome import ChromeDriverManager
 
 
-def validate_url(url):
+def validate_url(url: str) -> bool:
 
     if not isinstance(url, str):
         raise TypeError    
@@ -25,7 +29,7 @@ def validate_url(url):
     
     return True
 
-def extract_images(target_url):
+def extract_images(target_url: str) -> None:
     
     validate_url(target_url)
     
@@ -39,10 +43,44 @@ def extract_images(target_url):
                               chrome_options=chrome_options)
     
     try:
+        
         driver.get(target_url)
-        driver.get_screenshot_as_file('capture.png')
+        image_list = driver.find_elements_by_tag_name('img')
+                     
+        for img in image_list:
+            
+            img_src = get_element_src(img)
+            
+            if img_src is not None:
+            
+                img_name = img_src.rpartition('/')[-1]
+
+                # with open('out/{}'.format(img_name), 'wb') as save_location:
+
+                #     save_location.write(img.screenshot_as_png)
+                
+                try:
+                    urlretrieve(img_src, 'out/{}'.format(img_name))
+                except:
+                    print('Could not retrieve image \'{}\'.'.format(img_name))
+                    
         driver.close()
-    except:
-        raise ValueError
+
+    except Exception:
+        
+        traceback.print_exc()
+
+    driver.quit()
     
     return None
+
+def get_element_src(element: WebElement) -> str:
+    
+    if element.get_attribute('src'):
+        return element.get_attribute('src')
+    
+    elif element.get_attribute('data-src'):
+        return element.get_attribute('data-src')
+        
+    else:
+        return None
