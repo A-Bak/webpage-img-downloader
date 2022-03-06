2846c162b76df580250284349ef4e43c48e62b90
Adam Bak <59940328+A-Bak@users.noreply.github.com>
2022-03-06 19:38:36 +0100

    Added - navigation, scraping, downloading using Instructions.

--- wid/web/bot/webcrawler.py
+++ wid/web/bot/webcrawler.py
@@ -1,6 +1,6 @@
 from typing import List, Callable
 
-import re
+import os
 from collections import deque
 
 from selenium.common.exceptions import WebDriverException
@@ -19,8 +19,7 @@ from web.bot.instructions import Instructions
 
 class WebCrawler():
     
-
-    def __init__(self, instructions: Instructions, starting_url: Url, target_dir: str='./', img_regex: str=None) -> None:
+    def __init__(self, instructions: Instructions, starting_url: Url, target_dir: str='./wid-images') -> None:
 
         self.webdriver = None
         self.instructions = instructions
@@ -29,30 +28,23 @@ class WebCrawler():
         self.url_visited = set()
 
         self.target_dir = target_dir
-        self.img_pattern = re.compile(img_regex) if img_regex is not None else None
+
                 
-  
   
     def crawl(self) -> None:
         
         self.webdriver = web.img.scrape.initialize_webdriver()
-        
-        if self.instructions.validate is not None:
-            self.instructions.validate(self.webdriver, self.url_queue[0])
+        self.instructions.validate(self.webdriver, self.url_queue[0])
         
         while len(self.url_queue) > 0:
             target_url = self.url_queue.popleft()
-            
             print('Starting to parse web page \'{}\''.format(target_url))
             
             try:
-                # Navigate to next web page, find src URLs of image elements, filter image URLs, download images
                 self._navigate_url(target_url)
-                # image_urls = self._find_image_elements(target_url)
-                # filterd_image_urls = Url.filter_url_list(image_urls, self.img_pattern)
-                # web.img.save.save_images(filterd_image_urls, self.target_dir)
-                
-                # Get next set of URLs from current web page
+                image_urls = self._find_image_urls(target_url)
+                self._download_images(image_urls)
+
                 next_urls = self._get_next_url_list(target_url)
                 self.url_queue.extend(next_urls)
                    
@@ -60,6 +52,7 @@ class WebCrawler():
                 # Failed to load next web page -> continue with next in queue 
                 continue
             
+        self.webdriver.quit()            
         print('Done.')
             
             
@@ -79,23 +72,32 @@ class WebCrawler():
     def _get_next_url_list(self, current_url: Url, step_function: Callable[[Url], List[Url]]=None) -> List[Url]:
         
         if step_function is None:
-            next_url_list = self.instructions.next_step(self.webdriver, current_url)
+            next_url_list = self.instructions.next_step(self.webdriver)
         else:
-            step_function(self.webdriver, current_url)
+            step_function(self.webdriver)
 
         valid_url_list = [url for url in next_url_list if url.is_valid()]
         return [url for url in valid_url_list if url not in self.url_visited]
 
     
     
-    def _find_image_elements(self, visited_page_url: Url) -> List[Url]:
-        # visited_page_url -> for extracting basename in case of relative src URLs of elements
+    def _find_image_urls(self, visited_page_url: Url) -> List[Url]:
         if self.webdriver is None:
             raise ValueError('WebDriver is not initialized in WebCrawler.')    
         
-        image_elements = self.webdriver.find_elements_by_tag_name('img')
+        image_elements = self.instructions.find_image_elements(self.webdriver)
         return [web.img.scrape.get_element_src(e, visited_page_url) for e in image_elements]
     
     
+    
+    def _download_images(self, image_urls: List[Url]) -> None:
+        if self.webdriver is None:
+            raise ValueError('WebDriver is not initialized in WebCrawler.') 
+
+        url_dir_name = self.webdriver.current_url.split('/')[-2]
+        dir_path = os.path.join(self.target_dir, url_dir_name)
+        web.img.save.save_images(image_urls, dir_path)
+        
+        
 
     
