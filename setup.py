2a7b6a9c3ed2c1bb280c8b4d648bb75422ad66fa
Adam Bak <59940328+A-Bak@users.noreply.github.com>
2022-03-08 15:05:56 +0100

    Deployment

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
 import setuptools
 
+with open('ReadMe.md', 'r') as f:
+    long_description = f.read()
+    
+with open('requirements.txt', 'r', encoding='UTF-16') as f:
+    required = f.readlines()
+
+
 setuptools.setup(
-    name="Webpage-Image-Downloader",
-    version="0.0.1",
-    author="ABak",
+    name="wid",
+    version="1.0.0",
+    description="Tool for extracting and saving specific images from websites.",
+    long_description=long_description,
+    author="A-Bak",
     author_email="adam.bak753@gmail.com",
-    description="Tool for extracting and saving specific images from a website.",
     packages=["wid"],
+    install_requires=required,
 )
